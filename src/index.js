a7232be5bdacc7a2c6df1051eec5a63cbe9b5abe
Adepusrikanth9666 <64652479+Adepusrikanth9666@users.noreply.github.com>
2020-12-22 00:14:26 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,94 @@
 const express = require('express')
 const app = express()
 const port = 8080
-
+const {newsArticleModel} = require('./connector')
 const onePageArticleCount = 10
 
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+console.log(newsArticleModel)
+
+
+app.get("/newFeeds",(req,res)=>{ 
+
+    let  limitRecived = (req.query.limit);
+    let offsetRecived = (req.query.offset);
+
+    let limit   = parseInt(Number(limitRecived));
+    let offset  = parseInt(Number(offsetRecived));
+
+    console.log((limit),(offset));
+    console.log(isNaN(limit),isNaN(offset));
+    if(!limitRecived){
+        limit = onePageArticleCount;
+
+    }
+
+    if(!offsetRecived){
+
+        offset=0;
+    }
+
+
+    if(isNaN(offset)){
+
+        offset=0;
+    }
+
+    if(isNaN(limit)||limit<0){
+        limit = onePageArticleCount;
+
+        if(!offsetRecived){
+            offset=0;
+        }
+    }
+    
+
+    const values = {};
+    values.skip = offset;
+    values.limit = limit;
+
+ 
+    newsArticleModel.countDocuments({},function(error){
+        if(error){
+            res.status(500).send({message:error.message});
+            return;
+        }
+
+        newsArticleModel.find({},{},values,function(error,result){
+            
+
+            if(error){
+                res.status(500).send({message:error.message});
+                return;
+               
+
+            }
+            else{
+
+                res.status(200).send(result);
+               
+
+            }
+            
+        })
+        
+      
+
+    })
+
+  
+
+
+
+});
+
+
+
 
 
 
